40f72d6f98dd6a65832cd9ca4ac18b3c593ac70d
Tay4ang <guilherme-ro1989@hotmail.com>
2024-02-15 15:11:02 -0300

    divisão de valores

--- calculadora-de-imc.py
+++ calculadora-de-imc.py
@@ -3,6 +3,8 @@ altura = input("Qual é sua altura?:")
 peso = input("Qual é seu peso?:")
 
 #conversão de valores
-altura = float(altura)/100
+altura = float(altura)/100*2
 peso = int(peso)
 
+#divisão de valores
+IMC = peso/altura
