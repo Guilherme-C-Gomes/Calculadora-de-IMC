62488708a78ea7abd78a97bba38576c32bc15e5f
Tay4ang <guilherme-ro1989@hotmail.com>
2024-02-16 08:39:03 -0300

    limpeza e otimização do codigo

--- calculadora-de-imc.py
+++ calculadora-de-imc.py
@@ -1,25 +1,30 @@
 #entrada de dados
-altura = input("Qual é sua altura?:")
-peso = input("Qual é seu peso?:")
+altura = float(input("Qual é sua altura?:"))
+peso = float(input("Qual é seu peso?:"))
 
 #conversão de valores
-altura = float(altura)/100*2
+altura = altura/100
 peso = int(peso)
 
-#divisão de valores
-IMC = peso/altura
+#calculo do IMC
+IMC = peso / altura **2
 
 #dados de comparação 
-Abaixo_do_peso = 18,5
-acima_Peso_normal = 24,9
-Sobrepeso = 29,9
-obesidade1 = 34,9
+Abaixo_do_peso = 18.5
+acima_Peso_normal = 24.9
+Sobrepeso = 29.9
+obesidade1 = 34.9
 obesidade2e3 = 40
 
 #resultado do IMC
-print("Seu IMC é:"+IMC)
-if Abaixo_do_peso < IMC:
+print("Seu IMC é:{:.2f}".format(IMC))
+if Abaixo_do_peso <= IMC:
     print("Abaixo do peso")
-elif Abaixo_do_peso < IMC > acima_Peso_normal:
+elif Abaixo_do_peso <= IMC > acima_Peso_normal:
     print("Peso normal")
-elif acima_Peso_normal< 
+elif acima_Peso_normal<= IMC >Sobrepeso:
+    print("Sobrepeso")
+elif Sobrepeso<= IMC >obesidade1:
+    print("obesidade 1")
+else:
+    print("Obsidade 2 e 3")
